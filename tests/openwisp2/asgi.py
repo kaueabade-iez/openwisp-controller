a2a0792d4a71dbef57eb0d1a412443f6fe6cf837
Kauê Abade <kaue.silva@iez.com.br>
2026-06-19 17:17:25 -0300

    [qa] Ignoring "noqa: E402" on this file due to runtime errors otherwise

--- tests/openwisp2/asgi.py
+++ tests/openwisp2/asgi.py
@@ -2,11 +2,11 @@ from django.core.asgi import get_asgi_application
 
 django_asgi_app = get_asgi_application()
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack  # noqa: E402
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from channels.security.websocket import AllowedHostsOriginValidator  # noqa: E402
 
-from openwisp_controller.routing import get_routes
+from openwisp_controller.routing import get_routes  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
